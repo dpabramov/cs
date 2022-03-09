1192b69d143ba35b34d37bacaed8a43f0e15109a
Dmitry <dmitr.abramov2015@yandex.ru>
2022-03-09 18:24:19 +0300

    Start commit

---
+++ Calculation/SimpleCalculation.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Calculation.Types;
+
+namespace Calculation
+{
+    public static class SimpleCalculation
+    {
+        public static TypeOperation GetTypeOperation()
+        {
+            TypeOperation typeOperation;
+        Start:
+            Console.Write("Введите тип операции(0 - сложение, 1- вычитание, 2-умножение, 3 -деление, 4-возведение в степень, 5 - остаток от деления): ");
+
+            try
+            {
+                typeOperation = (TypeOperation)Enum.Parse(typeof(TypeOperation), Console.ReadLine());
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Введено некорректное значение... Повторите ввод.");
+                goto Start;
+            }
+            catch
+            {
+                Console.WriteLine("Что-то не то... Повторите ввод.");
+                goto Start;
+            }
+
+
+            return typeOperation;
+        }
+
+        public static double GetValueFromConsole()
+        {
+            double val1;
+
+        start:
+            Console.Write("Введите число: ");
+            string str = Console.ReadLine();
+
+            try
+            {
+                val1 = Convert.ToDouble(str);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Входная строка имела неверный формат.\nПовторите попытку ввода...");
+                goto start;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Слишком большое число.\nПовторите попытку ввода...");
+                goto start;
+            }
+            catch
+            {
+                Console.WriteLine("Вообще какая-то хрень.\nПовторите попытку ввода...");
+                goto start;
+            }
+
+            return val1;
+        }
+
+        public static void Calculate(double val1, double val2, TypeOperation typeOperation)
+        {
+            switch (typeOperation)
+            {
+                case TypeOperation.Add:
+                    Console.WriteLine("{0} + {1} = {2}", val1, val2, val1 + val2);
+                    break;
+                case TypeOperation.Minus:
+                    Console.WriteLine("{0} - {1} = {2}", val1, val2, val1 - val2);
+                    break;
+                case TypeOperation.Multiply:
+                    Console.WriteLine("{0} * {1} = {2}", val1, val2, val1 * val2);
+                    break;
+                case TypeOperation.Divide:
+                    Console.WriteLine("{0} / {1} = {2}", val1, val2, val1 / val2);
+                    break;
+                case TypeOperation.Pow:
+                    Console.WriteLine("{0} ^ {1} = {2}", val1, val2, Math.Pow(val1, val2));
+                    break;
+                case TypeOperation.Procent:
+                    Console.WriteLine("{0} % {1} = {2}", val1, val2, val1 % val2);
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная операция");
+                    break;
+            }
+        }
+    }
+
+    public static class Types
+    {
+        public enum TypeOperation
+        {
+            Add,
+            Minus,
+            Multiply,
+            Divide,
+            Pow,
+            Procent
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Calculation.SimpleCalculation;
+using static Calculation.Types;
+
+namespace ConsoleApp2
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string con;
+            ConsoleKeyInfo consoleKeyInfo;
+            do
+            {
+                double val1 = GetValueFromConsole();
+                double val2 = GetValueFromConsole();
+                TypeOperation typeOperation = GetTypeOperation();
+
+                Calculate(val1, val2, typeOperation);
+
+                Console.Write("Продолжить (Y/N)? ");
+                consoleKeyInfo = Console.ReadKey();
+                Console.WriteLine("\n");
+            } while (consoleKeyInfo.Key.ToString() == "y" || consoleKeyInfo.Key.ToString() == "Y");
+        }
+    }
+}
