feabfb71a5cce5695ddfb30c18e77101365982c7
Dmitry <dmitr.abramov2015@yandex.ru>
2022-03-10 13:27:24 +0300

    Fourth commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -17,12 +17,18 @@ namespace ConsoleApp2
             do
             {
                 double val1 = GetValueFromConsole();
+				
                 double val2 = GetValueFromConsole();
-                TypeOperation typeOperation = GetTypeOperation();
-                Calculate(val1, val2, typeOperation);
-                Console.Write("Продолжить (Y/N)? ");
-                consoleKeyInfo = Console.ReadKey();
-                Console.WriteLine("\n");
+                
+				TypeOperation typeOperation = GetTypeOperation();
+                
+				Calculate(val1, val2, typeOperation);
+                
+				Console.Write("Продолжить (Y/N)? ");
+                
+				consoleKeyInfo = Console.ReadKey();
+                
+				Console.WriteLine("\n");
             } while (consoleKeyInfo.Key.ToString() == "y" || consoleKeyInfo.Key.ToString() == "Y");
         }
     }
